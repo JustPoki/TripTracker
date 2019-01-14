24c3cc91a458adfe94c6292122ceda84c1023bab
Net-Poki <Net-Poki@DESKTOP-5D5NQ0U>
2019-01-14 06:46:13 +0100

    First module with Jeffrey

---
+++ TripTracker.BackService/Controllers/TripsControler.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using TripTracker.BackService.Models;
+
+namespace TripTracker.BackService.Controllers
+{
+	[Route("api/[controller]")]
+	public class TripsController : Controller
+	{
+
+		public TripsController(Models.Repository repository)
+		{
+			_repository = repository;
+		}
+
+		private Models.Repository _repository;
+
+		// GET api/Trips
+		[HttpGet]
+		public IEnumerable<Models.Trip> Get()
+		{
+			return _repository.Get();
+		}
+
+		// GET api/Trips/5
+		[HttpGet("{id}")]
+		public Trip Get(int id)
+		{
+			return _repository.Get(id);
+		}
+
+		// POST api/Trips
+		[HttpPost]
+		public void Post([FromBody]Trip value)
+		{
+			_repository.AddTrip(value);
+		}
+
+		// PUT api/Trips/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody]Trip value)
+		{
+			_repository.AddTrip(value);
+		}
+
+		// DELETE api/Trips/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+			_repository.Remove(id);
+		}
+	}
+}
---
+++ TripTracker.BackService/Models/Repository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TripTracker.BackService.Models
+{
+	public class Repository
+	{
+		private List<Trip> MyTrips = new List<Trip>
+		{
+			new Trip
+			{
+				Id= 1,
+				Name="MVP Summit",
+				StartDate = new DateTime(2018,3,5),
+				EndDate = new DateTime(2018,3,8)
+			},
+			new Trip
+			{
+				Id=2,
+				Name="Dev Intersection Orlando",
+				StartDate = new DateTime(2018,3,25),
+				EndDate = new DateTime(2018,3,27)
+			},
+			new Trip
+			{
+				Id = 2,
+				Name = "Build 2018",
+				StartDate = new DateTime(2018,5,7),
+				EndDate = new DateTime(2018,5,10)
+			}
+		};
+
+		public List<Trip> Get()
+		{
+			return MyTrips;
+		}
+
+		public Trip Get(int id)
+		{
+			return MyTrips.First(t=> t.Id == id);
+		}
+
+		public void AddTrip(Trip newTrip)
+		{
+			MyTrips.Add(newTrip);
+		}
+
+		public void EditTrip(Trip tripToUpdate)
+		{
+			MyTrips.Remove(MyTrips.First(t => t.Id == tripToUpdate.Id));
+			MyTrips.Add(tripToUpdate);
+		}
+
+		public void Remove(int id)
+		{
+			MyTrips.Remove(MyTrips.First(t => t.Id == id));
+		}
+
+	}
+}
